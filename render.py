74bae048e12f2af7805fed57f19ed68aa8447b86
Ben Webb <ben.webb@opendataservices.coop>
2022-07-17 22:56:18 +0100

    Create thumbnails

--- render.py
+++ render.py
@@ -1,4 +1,7 @@
 import json
+import os
+import shutil
+import subprocess
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -9,12 +12,33 @@ template = env.get_template("template.html")
 with open("exported_json/content/posts_1.json") as fp:
     posts = json.load(fp)
 
+
 for post in posts:
     if len(post["media"]) == 1:
         for key in ["title", "creation_timestamp"]:
             post[key] = post["media"][0][key]
+    for media in post["media"]:
+        os.makedirs(os.path.join("out", os.path.dirname(media["uri"])), exist_ok=True)
+        original_file = os.path.join("exported_json", media["uri"])
+        copied_file = os.path.join("out", media["uri"])
+        media["thumb_uri"] = media["uri"] + ".thumb.jpg"
+        if False:
+            shutil.copy(original_file, copied_file)
+            subprocess.run(
+                [
+                    "ffmpeg",
+                    "-i",
+                    original_file,
+                    "-vf",
+                    "scale=320:320:force_original_aspect_ratio=decrease",
+                    "-vframes",
+                    "1",
+                    os.path.join(out, media["thumb_uri"]),
+                ],
+                check=True,
+            )
 
-posts.sort(key=lambda post: post["creation_timestamp"])
+posts.sort(key=lambda post: -post["creation_timestamp"])
 
-with open("out.html", "w") as fp:
+with open(os.path.join("out", "index.html"), "w") as fp:
     fp.write(template.render(posts=posts))
